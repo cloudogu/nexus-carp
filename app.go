bcccf1df6ec854a47a88f14a3bc4313781b6b34b
Sebastian Sdorra <sebastian.sdorra@triology.de>
2018-04-17 16:34:21 +0200

    wait until nexus is fully up and running

--- app.go
+++ app.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/cloudogu/carp"
+	"github.com/cloudogu/go-health"
 	"github.com/golang/glog"
 )
 
@@ -20,6 +21,12 @@ func main() {
 	configuration, err := carp.ReadConfiguration()
 	if err != nil {
 		glog.Fatal("failed to read configuration:", err)
+	}
+
+	glog.Infof("wait until nexus is ready")
+	err = waitUntilNexusBecomesReady(url, username, password)
+	if err != nil {
+		glog.Fatal("nexus does not become ready:", err)
 	}
 
 	glog.Infof("start nexus-carp %s", Version)
@@ -46,4 +53,22 @@ func env(key string) string {
 		glog.Fatalf("environment variable %s is not set", key)
 	}
 	return value
+}
+
+func waitUntilNexusBecomesReady(url string, username string, password string) error {
+	checker := health.NewHTTPHealthCheckBuilder(url+"/service/metrics/healthcheck").
+		WithMethod("GET").
+		WithBasicAuth(username, password).
+		Build()
+
+	watcher := health.NewWatcher()
+	watcher.RecheckLimit = 300
+	watcher.ResultListener = func(counter int, err error) {
+		glog.Infof("nexus health check number %v failed, still waiting until nexus becomes ready", counter)
+	}
+	err := watcher.WaitUntilHealthy(checker)
+	if err != nil {
+		return err
+	}
+	return nil
 }
