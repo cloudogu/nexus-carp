e52d7f8f53089222b9088c66473107750db4655a
Vincent <vincent.poneleit@cloudogu.com>
2022-03-07 15:55:54 +0100

    #14 added parsing of cesapp loglevel for internal glog logger

--- app_test.go
+++ app_test.go
@@ -76,3 +76,29 @@ func TestIsJSInjectionRequiredMissingContentType(t *testing.T) {
 
 	assert.False(t, injectRequired)
 }
+
+const succeed = "\u2713"
+const failed = "\u2717"
+
+func TestMapCesappLoglevelToGlogLoglevel(t *testing.T) {
+	tables := []struct {
+		name                string
+		givenCesappLogLevel string
+		expectedGlogLevel   int
+	}{
+		{"TestForError", "ERROR", 0},
+		{"TestForWarn", "WARN", 1},
+		{"TestForInfo", "INFO", 2},
+		{"TestForDebug", "DEBUG", 5},
+	}
+
+	for _, table := range tables {
+		receivedGlogLogLevel := mapCesappLoglevelToGlogLoglevel(table.givenCesappLogLevel)
+		if table.expectedGlogLevel != receivedGlogLogLevel {
+			log.Errorf("\t%s: should return logLevel: %v, got: %v", failed, table.expectedGlogLevel, receivedGlogLogLevel)
+		} else {
+			t.Logf("\t%s: logLevel should be accurate.", succeed)
+		}
+	}
+
+}
